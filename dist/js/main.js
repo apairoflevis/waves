10682d2d5075073f9ab7310a9b2713bfc4abdd03
Andrew <delapaz10@gmail.com>
2019-12-16 20:43:36 -0600

    testing

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,13 +20,3 @@ function myTimer() {
     var d = new Date();
     document.getElementById("time").innerHTML = d.toLocaleTimeString();
 }
-
-//Project Clock
-var myVar = setInterval(function () {
-    myTimer();
-}, 1000);
-
-function myTimer() {
-    var d = new Date();
-    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
-}
